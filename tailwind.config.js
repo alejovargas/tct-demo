30f56b13be3e6926e4ed95c12f300196efa9c035
Alejandro Vargas <alejo@me.com>
2023-04-19 12:14:59 -0400

    fix: Nuxt 3 with StoryBlok working stable

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "storyblok/**/*.{vue,js}",
+    "components/**/*.{vue,js}",
+    "pages/**/*.vue",
+  ],
+  theme: {
+    fontFamily: {
+      sans: "Roboto, sans-serif",
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    // require("@tailwindcss/line-clamp"),
+  ],
+};
